3a1973090fb689055d834b9f60662335d91e3e46
lea.arturi <lea.arturi@gmail.com>
2021-06-05 19:00:12 -0300

    Comunicacion frontend con socket y validacion de token

--- models/sockets.js
+++ models/sockets.js
@@ -1,15 +1,23 @@
+const { comprobarJWT } = require("../helpers/jwt");
 class Sockets {
 
     constructor( io ) {
-
         this.io = io;
-
         this.socketEvents();
     }
 
     socketEvents() {
         // On connection
         this.io.on('connection', ( socket ) => {
+
+            const [ valido, uid ] = comprobarJWT(socket.handshake.query['x-token']);
+
+            if (!valido) {
+                console.log('Socket no identificado. JWT no valido.');
+                return socket.disconnect();
+            }
+
+            console.log('Cliente conectado', uid);
 
            // Validar el JWT
            // Si el token no es valido, desconectar
@@ -25,6 +33,10 @@ class Sockets {
            // Desconectar usuario y marcar en la BD
 
            // Emitir todos los usuarios conectados
+
+           socket.on('disconnect', () => {
+               console.log('Cliente desconectado');
+           })
         
         });
     }
